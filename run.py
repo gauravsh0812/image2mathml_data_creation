6f047f2366f7f815f9954a92c5e4c07ca8bffb42
gauravsh0812 <gauravs@arizona.edu>
2023-03-07 20:47:00 -0700

    update

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from creating_im2mml_latex_nK.create_im2mml_nK import latex2mml as l2m
-from creating_im2mml_latex_nK.create_im2mml_nK import im2mml_2_im2latex as m2l
+from creating_im2mml_latex_nK.create_im2latex_nK import im2mml_2_im2latex as m2l
 
 
 if __name__=="__main__":
