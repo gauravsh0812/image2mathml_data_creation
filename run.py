92d330f7976b695d3ce532022ef56f9f0a9c65ca
gauravsh0812 <gauravs@arizona.edu>
2023-03-08 23:29:36 -0700

    update

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 from creating_im2mml_latex_nK.create_im2mml_nK import latex2mml as l2m
 from creating_im2mml_latex_nK.create_im2latex_nK import im2mml_2_im2latex as m2l
-
+from creating_im2mml_latex_nK.create_im2latex_nK import create_data
 
 if __name__=="__main__":
     # render im2mml from im2latex-103K
-    l2m()
+    # l2m()
 
     # grab the corresponding latex from im2latex-103K
     # m2l()
+
+    # create data for cnn_xfmer
+    create_data()
