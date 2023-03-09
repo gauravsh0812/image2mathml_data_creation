fbd1e43e582d2647d3d9406ed53863e56ca4a028
gauravsh0812 <gauravs@arizona.edu>
2023-03-08 23:30:59 -0700

    update

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from creating_im2mml_latex_nK.create_im2mml_nK import latex2mml as l2m
 from creating_im2mml_latex_nK.create_im2latex_nK import im2mml_2_im2latex as m2l
-from creating_im2mml_latex_nK.create_im2latex_nK import create_data
+from creating_im2mml_latex_nK.create_data_for_cnn_xfmer import create_data
 
 if __name__=="__main__":
     # render im2mml from im2latex-103K
