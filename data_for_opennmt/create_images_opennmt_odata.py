ac4259ff175c23482ba20d0dadb4d5353c82590b
gauravsh0812 <gauravs@arizona.edu>
2023-05-23 11:17:28 -0400

    add

--- data_for_opennmt/create_images_opennmt_odata.py
+++ data_for_opennmt/create_images_opennmt_odata.py
@@ -5,11 +5,11 @@ def no_blank_data():
     images = "/home/skema/img2mml/gauravs_data_for_paper/data/odata/our_sampled_data/images"
     new_images = "/home/skema/img2mml/gauravs_data_for_paper/data/opennmt/oimages"
     blank =  open("/home/skema/img2mml/gauravs_data_for_paper/data/opennmt/our_blank_images.txt").readlines()
-
+    blank_idx = [int(i.split(".")[0]) for i in blank]
 
     count = 0
     for i in range(len(os.listdir(images))):
-        if str(i)+".png" not in blank or str(i)+".png\n" not in blank:
+        if i not in blank_idx:
             # if i % 1000 ==0: print(i)
             src = os.path.join(images, str(i)+".png")
             dst = os.path.join(new_images, str(count)+".png")
@@ -25,5 +25,5 @@ def no_blank_data():
     new_latex = open("/home/skema/img2mml/gauravs_data_for_paper/data/odata/our_sampled_data/no_blank_original_latex.lst", "w")
 
     for i in range(len(org_latex)):
-        if str(i)+".png" not in blank:
+        if i not in blank_idx:
             new_latex.write(org_latex[i])
