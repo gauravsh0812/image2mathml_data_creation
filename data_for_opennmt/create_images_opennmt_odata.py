161740ebf43c7c31ed3b80ad9859fb42576366da
gauravsh0812 <gauravs@arizona.edu>
2023-05-23 10:37:03 -0400

    update

--- data_for_opennmt/create_images_opennmt_odata.py
+++ data_for_opennmt/create_images_opennmt_odata.py
@@ -5,14 +5,14 @@ def no_blank_data():
     images = "/home/skema/img2mml/gauravs_data_for_paper/data/odata/our_sampled_data/images"
     new_images = "/home/skema/img2mml/gauravs_data_for_paper/data/opennmt/oimages"
     blank = open("/home/skema/img2mml/gauravs_data_for_paper/data/opennmt/our_blank_images.txt").readlines()
-
-    count = 0
-    for i in range(len(os.listdir(images))):
-        if str(i)+".png" not in blank:
-            if i % 1000 ==0: print(i)
-            src = os.path.join(images, str(i)+".png")
-            dst = os.path.join(new_images, str(count)+".png")
-            shutil.copyfile(src, dst)
+    # 
+    # count = 0
+    # for i in range(len(os.listdir(images))):
+    #     if str(i)+".png" not in blank:
+    #         if i % 1000 ==0: print(i)
+    #         src = os.path.join(images, str(i)+".png")
+    #         dst = os.path.join(new_images, str(count)+".png")
+    #         shutil.copyfile(src, dst)
 
     # -------------------------------------------------------------------------------
     # redefining original-latex file by eliminating latex correspoding to blank file.
